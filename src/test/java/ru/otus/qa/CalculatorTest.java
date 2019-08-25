534082a9a7e7bb9b465de69000af32c0b0b90ba3
lubnin <lubnin@users.noreply.github.com>
2019-08-25 16:24:48 +0300

    Lesson13

--- src/test/java/ru/otus/qa/CalculatorTest.java
+++ src/test/java/ru/otus/qa/CalculatorTest.java
@@ -3,30 +3,60 @@ package ru.otus.qa;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import ru.otus.qa.service.CalculatorService;
+import ru.otus.qa.web.CalculatorController;
 
-import static io.restassured.RestAssured.get;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(ru.otus.qa.service.CalculatorService.class)
 class CalculatorTest {
-
-    @LocalServerPort
-    private int port;
+    
+    private MockMvc mockMvc;
 
     @BeforeEach
     void setup() {
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = port;
+        CalculatorController controller = new CalculatorController(new CalculatorService());
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
-    void testPlus() {
-        int result = get("/calc?operation=plus&a=5&b=3").then().statusCode(200)
-                .assertThat().log().all()
-                .extract().body().as(Integer.class);
-
-        assertEquals(8, result);
+    void testPlus() throws Exception{
+        String result = mockMvc.perform(get("/calc?operation=plus&a=5&b=3"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("8", result);
+    }
+    @Test
+    void testMinus() throws Exception{
+        String result = mockMvc.perform(get("/calc?operation=minus&a=2&b=4"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("-2", result);
+    }
+    @Test
+    void testMultiply() throws Exception{
+        String result = mockMvc.perform(get("/calc?operation=multiply&a=165&b=-15"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("-2475", result);
+    }
+    @Test
+    void testDivide() throws Exception{
+        String result = mockMvc.perform(get("/calc?operation=divide&a=555&b=5"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("111", result);
     }
 }
